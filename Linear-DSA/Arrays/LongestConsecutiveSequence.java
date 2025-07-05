075ed8d52ade5593861e40a9e86892da88cc76ea
ALAN-KRATI <alankratisaxena@gmail.com>
2025-07-05 17:37:30 +0530

    added solution for Longest Consecutive Sequence using HashSet to achieve O(n) time complexity

---
+++ Linear-DSA/Arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,19 @@
+class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        int maxLen = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int curr = num, len = 1;
+                while (set.contains(++curr)) len++;
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println(new LongestConsecutiveSequence().longestConsecutive(nums));
+    }
+}
