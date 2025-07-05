84ed8dad47f0c1de282943b7f75abd2f7bbe9976
ALAN-KRATI <alankratisaxena@gmail.com>
2025-07-05 17:37:30 +0530

    added solution for Product Array Except Self using prefix and suffix arrays to compute result in O(n) time without division

---
+++ Linear-DSA/Arrays/ProductArrayExceptSelf.java
@@ -0,0 +1,31 @@
+class ProductArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ProductArrayExceptSelf obj = new ProductArrayExceptSelf();
+        int[] nums = {1, 2, 3, 4};
+        int[] result = obj.productExceptSelf(nums);
+        for (int num : result) System.out.print(num + " ");
+    }
+}
