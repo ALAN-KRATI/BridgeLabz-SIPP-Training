16eb9c46f1eb611d35084804efc1927e568e6f5d
ALAN-KRATI <alankratisaxena@gmail.com>
2025-08-27 10:56:23 +0530

    Deleted

--- Functional-Interface/Background.java
+++
@@ -1,6 +0,0 @@
-public class BackgroundJob {
-    public static void main(String[] args) {
-        Runnable task = () -> System.out.println("Executing background task");
-        new Thread(task).start();
-    }
-}
--- Functional-Interface/Temperature.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Predicate;
-
-public class TemperatureAlert {
-    public static void main(String[] args) {
-        Predicate<Double> isHigh = t -> t > 37.5;
-        System.out.println(isHigh.test(38.0));
-        System.out.println(isHigh.test(36.5));
-    }
-}
